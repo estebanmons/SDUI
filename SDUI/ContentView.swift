12f03144d76c012387cdf837c0592ed79d7d8c44
Esteban Monsalve <esteban_mons@outlook.com>
2024-02-01 12:31:03 -0500

    App

---
+++ SDUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDUI
+//
+//  Created by Esteban Monsalve on 1/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SDUI/SDUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDUIApp.swift
+//  SDUI
+//
+//  Created by Esteban Monsalve on 1/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SDUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
